0e10005e03724ba184850029b63a051f70890689
Xin Hao <126965580+SheenHao8023@users.noreply.github.com>
2024-09-28 12:31:41 +0800

    Update WaveletCoherence&IBS.m

--- Wavelet_Coherence.m
+++ Wavelet_Coherence.m
@@ -1,7 +1,25 @@
-for i = 1:12
-for j = 1:12
-[wcoh,wcs,f] = wcoherence(nirsdata.oxyData(:,i) , nirsdata.oxyData(:,j));
-A = wcoh (find(0.01<=f(:,1) & f(:,1)<=0.04) , :);
-data(i,j) = mean(A(:));
-end
+folderPath1 = 'C:\Users\ASUS\Desktop\SCZ_tACS\data\A\ROI\EO_HC'; 
+folderPath2 = 'C:\Users\ASUS\Desktop\SCZ_tACS\data\B\ROI\EO_HC'; 
+fileList1 = dir(fullfile(folderPath1, 'S*.mat'));
+fileList2 = dir(fullfile(folderPath2, 'S*.mat'));
+coherence_matrix = zeros(12, 12);
+for i = 1:length(fileList1)
+    filePath1 = fullfile(folderPath1, fileList1(i).name);
+    [~, name, ~] = fileparts(filePath1);
+    data1=load(filePath1); 
+    filePath2 = fullfile(folderPath2, fileList2(i).name);
+    data2=load(filePath2); 
+        for m = 1:12
+            for n = 1:12
+                data1_col = data1.nirsdata.oxyData(:, m);
+                data2_col = data2.nirsdata.oxyData(:, n);
+                min_length = min(length(data1_col), length(data2_col));
+                data1_col = data1_col(1:min_length);
+                data2_col = data2_col(1:min_length);
+                [wcoh,wcs,f] = wcoherence(data1_col, data2_col);  %intra-brain等长，inter-brain不等长
+                coherence_value = wcoh (find(0.01<=f(:,1) & f(:,1)<=0.04) , :);
+                coherence_matrix(m,n) = mean(coherence_value(:));
+            end
+        end
+    save(fullfile('C:\Users\ASUS\Desktop\SCZ_tACS\data\IBS\EO_HC', [name, '.mat']), 'coherence_matrix');
 end
