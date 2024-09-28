53ced037e3eb7f6c634d87744110e7388f052c28
Xin Hao <126965580+SheenHao8023@users.noreply.github.com>
2024-09-28 19:57:00 +0800

    Update write_coherence.m

--- write_coherence.m
+++ write_coherence.m
@@ -1,15 +1,19 @@
-folderPath = 'C:\Users\ASUS\Desktop\SCZ_tACS\ROI\FC_SZ\cutting\EO_SCZ\CORR';
-filePattern = fullfile(folderPath, 'EO*.mat');
-fileList = dir(filePattern);
+folderPath1 = 'C:\Users\ASUS\Desktop\SCZ_tACS\data\IBS\EO_SZ';
+fileList1 = dir(fullfile(folderPath1, 'S*.mat'));
 outputMatrix = [];
-
-for i = 1:length(fileList)
-    fileName = fullfile(folderPath, fileList(i).name);
-    data = load(fileName);
-    if isfield(data, 'data')
-        rowVector = data.data(:)';
-        outputMatrix = [outputMatrix; rowVector];
-    end
+for i = 1:length(fileList1)
+    fileName = fullfile(folderPath1, fileList1(i).name);
+    load(fileName);
+    row_coherence = coherence_matrix(:)';
+    outputMatrix = [outputMatrix; row_coherence];
+end
+folderPath2 = 'C:\Users\ASUS\Desktop\SCZ_tACS\data\IBS\EO_HC';
+fileList2 = dir(fullfile(folderPath2, 'S*.mat'));
+for j = 1:length(fileList2)
+    fileName = fullfile(folderPath2, fileList2(j).name);
+    load(fileName);
+    row_coherence = coherence_matrix(:)';
+    outputMatrix = [outputMatrix; row_coherence];
 end
 selected_output = outputMatrix(:, [2:12 15:24 28:36 41:48 54:60 67:72 80:84 93:96 106:108 119:120 132]);
-
+writematrix(selected_output, 'C:\Users\ASUS\Desktop\SCZ_tACS\data\IBS\EO.xlsx');
