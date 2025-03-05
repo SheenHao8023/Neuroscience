360ff83d54c5263955043a8c9af4d7462b9a5676
Xin Hao <126965580+SheenHao8023@users.noreply.github.com>
2025-03-05 20:02:12 +0800

    Delete Average_matrix.m

--- Average_matrix.m
+++
@@ -1,8 +0,0 @@
-D = dir('C:\Users\ASUS\Desktop\SCZ_tACS\ROI analysis\FC_SZ\cutting\HA_SCZ\CORR\*.mat');
-HA = zeros(12,12);
-for i = 1:length(D)
-load(['C:\Users\ASUS\Desktop\SCZ_tACS\ROI analysis\FC_SZ\cutting\HA_SCZ\CORR\' D(i).name]);
-data( find (isnan(data)==1)) = 0.5;
-HA = HA + data;
-end
-HA = HA / 23;
